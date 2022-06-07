c25e7e1dce297f02d120cb0a22aa5d5c51f568fb
unknown <carinnovello@gmail.com>
2022-06-06 23:57:18 -0300

    modif app.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 3000,
-    ()=>{console.log('Servidor funcionando')
+app.listen(process.env.PORT || 3000, function() {console.log('Servidor funcionando')
 });
 
 app.get('/', (req,res)=>{
